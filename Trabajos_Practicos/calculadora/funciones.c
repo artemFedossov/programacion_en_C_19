9dfdedf5e43bca5665133840281235c0e377a28f
artem <a.fedossov@hotmail.com.ar>
2019-03-27 12:14:44 -0300

    Empezando tp1

---
+++ Trabajos_Practicos/calculadora/funciones.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void menu(){
+
+    float numeroA = 0;
+    float numeroB = 0;
+    char opcionMenu = '5';
+
+    printf("----------CALCULADORA----------\n\n");
+
+    do{
+            printf("1) Ingrese el 1er operador (A=%.2f)\n",numeroA);
+            printf("2) Ingrese el 2do operador (B=%.2f)\n",numeroB);
+            printf("3) Calcular todas las operaciones\n");
+            printf("4) Informar resultados\n");
+            printf("5) Salir\n");
+            printf("Opcion: ");
+            scanf("%c",&opcionMenu);
+            opcionMenu =
+
+    }while(opcionMenu!=5);
+
+
+
+}
+
+int validarMenu(char opcionMenu){
+
+    while(opcionMenu)
+}
