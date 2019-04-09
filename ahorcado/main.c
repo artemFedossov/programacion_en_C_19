a0bf2938f60046f1256ebed43cb3f02270757f4e
Artem Fedossov <a.fedossov@hotmail.com.ar>
2019-04-09 13:33:00 -0300

    Terminado opciones de menu

--- ahorcado/main.c
+++ ahorcado/main.c
@@ -1,33 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    char opcion[10];
-    char eMensaje[2][51] = {"La opcion ingresada es incorrecta: "}
+    char opcionPrincipal[21];
+    char opcionSecundaria[21];
+    char menuPrincipal[4][31] = {"\t\tAHORCADO\n\n","1) A JUGAR\n","2) Salir\n","\nOpcion: "};
+    char menuJugar[5][31] = {"\t\tAHORCADO\n\n","1) Ingresar palabra\n","2) Adivinar palabra\n","3) Regresar\n""\nOpcion: "};
+    char eMensaje[3][51] = {"ERROR, opcion incorrecta: "," "};
 
-    char ptrOpcion;
+    char *ptrOpcionPrincipal;
+    char *ptrOpcionSecundaria;
 
-    ptrOpcion = &opcion[0];
+    ptrOpcionPrincipal = &opcionPrincipal[0];
+    ptrOpcionSecundaria = &opcionSecundaria[0];
 
     do{
 
-        printf("\n-----------------AHORCADO------------------\n\n");
-        printf("1) A Jugar\n");
-        printf("2) Salir\n");
-        printf("\nOpcion: ");
-        gets(ptrOpcion);
+        menu(menuPrincipal,4,eMensaje[0],ptrOpcionPrincipal,'1','2');
+        system("cls");
 
-        validarMenu
+        switch(*ptrOpcionPrincipal){
+            case '1':   menu(menuJugar,4,eMensaje[0],ptrOpcionSecundaria,'1','3');
+                        switch(*ptrOpcionSecundaria){
+                            case '1':   printf("hola");
+                                        system("pause");
+                                break;
+                        }
+                break;
+        }
 
-
-    }while(opcion != '2');
+    }while(*ptrOpcionPrincipal != '2');
 
 
     return 0;
 }
 
-void validarMenu(char *validar, int desde, int hasta, char eMensaje[]){
+void menu(char menu[][31],int filaMenu,char eMensaje[],char *opcion,char desde,char hasta){
 
+    char validar[21];
+    int i;
 
+    for(i=0; i <filaMenu; i++){
+
+        printf(menu[i]);
+    }
+
+    fflush(stdin);
+    scanf("%s",validar);
+
+    validarOpcion(eMensaje,validar,opcion,desde,hasta);
+
+}
+
+void validarOpcion(char eMensaje[],char validar[],char *opcion,char desde,char hasta){
+
+    int i=0;
+    int carracteres;
+
+    carracteres = strlen(validar);
+
+    while(validar[i] != '\0'){
+
+        if((validar[i] < desde || validar[i] > hasta) || (carracteres!=1)){
+
+            printf(eMensaje);
+            fflush(stdin);
+            scanf("%s",validar);
+            carracteres = strlen(validar);
+            i=0;
+            continue;
+        }
+        i++;
+    }
+
+    *opcion = validar[0];
 }
